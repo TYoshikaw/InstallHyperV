d0033ede423dc74a214f11ce0176d6bab3c800f0
TYoshikawa <20.toshihiko.yoshikawa@gmail.com>
2022-06-16 12:27:23 +0900

    updated on 2022.6.16

--- Install-Hyper-V.ps1
+++ Install-Hyper-V.ps1
@@ -5,20 +5,25 @@
 # Install Hyper-V Hypervisor and Hyper-V Management Tools
 #
 # Created on 2022.5.15
+# Updated on 2022.6.16
+#   Modified the code for readability
 #
 # Reference : https://www.altaro.com/hyper-v/install-hyper-v-powershell-module/
 #
 ####################################################################################################
 
 Get-WindowsFeature -Name *hyper-v* ##check status of Hyper-V Module
+Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart ##innstall Hyper-V Hypervisor and all tools (method #1)
 
+#### reference
 # Install-WindowsFeature -Name Hyper-V -Restart  ##install only Hyper-V Hypervisor
 # Uninstall-WindowsFeature -Name Hyper-V -Restart  ##uninstall only Hyper-V Hypervisor
 
 # Install-WindowsFeature -Name RSAT-Hyper-V-Tools ##install Hyper-V manager and PowerShell Module 
 # Uninstall-WindowsFeature -Name RSAT-Hyper-V-Tools ##uninstall Hyper-V manager and PowerShell Module
 
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart ##innstall Hyper-V Hypervisor and all tools (method #1)
+## The following line is used above 
+## Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart ##innstall Hyper-V Hypervisor and all tools (method #1)
 # Uninstall-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart ##uninstall Hyper-V Hypervisor and all tools (method #1)
 
 # Install-WindowsFeature -Name Hyper-V, RSAT-Hyper-V-Tools -Restart ##install Hyper-V Hypervisor and all tools (method #2)
